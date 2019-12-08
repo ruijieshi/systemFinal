f462197bf138f05eb04d4357ff95d429ffed7c5b
ruijieshi <shi.ruij@husky.neu.edu>
2019-12-08 12:42:57 -0800

    change comment

--- src/filesys/directory.h
+++ src/filesys/directory.h
@@ -5,10 +5,6 @@
 #include <stddef.h>
 #include "devices/block.h"
 
-/* Maximum length of a file name component.
-   This is the traditional UNIX maximum length.
-   After directories are implemented, this maximum length may be
-   retained, but much longer full path names must be allowed. */
 #define NAME_MAX 14
 
 struct inode;
